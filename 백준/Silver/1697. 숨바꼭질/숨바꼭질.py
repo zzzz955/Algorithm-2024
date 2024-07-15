3287e12b9f8a945bb5dce859d02436b909bd0820
Sang <106306044+zzzz955@users.noreply.github.com>
2024-07-15 14:28:42 +0900

    [Silver I] Title: 숨바꼭질, Time: 88 ms, Memory: 34244 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,32 +1,19 @@
 from collections import deque
 
-v = [False] * 100001
-n, k = map(int, input().split())
-dist = (-1, 1)
 
-
-def bfs(index):
-    q = deque([index])
-    v[index[0]] = True
+def bfs(n, k):
+    v = [False] * 100001
+    q = deque([(n, 0)])
+    v[n] = True
     while q:
         x, cnt = q.popleft()
         if x == k:
             return cnt
-        elif x < k:
-            x1, x2, x3 = x + dist[0], x + dist[1], x * 2
-            if 0 <= x1 < 100001 and not v[x1]:
-                v[x1] = True
-                q.append((x1, cnt + 1))
-            if 0 <= x2 < 100001 and not v[x2]:
-                v[x2] = True
-                q.append((x2, cnt + 1))
-            if 0 <= x3 < 100001 and not v[x3]:
-                v[x3] = True
-                q.append((x3, cnt + 1))
-        else:
-            x1 = x + dist[0]
-            if not v[x1] and 0 <= x1 < 100001:
-                q.append((x1, cnt + 1))
+        for nx in (x - 1, x + 1, x * 2):
+            if 0 <= nx < 100001 and not v[nx]:
+                v[nx] = True
+                q.append((nx, cnt + 1))
 
 
-print(bfs((n, 0)))
+n, k = map(int, input().split())
+print(bfs(n, k))
