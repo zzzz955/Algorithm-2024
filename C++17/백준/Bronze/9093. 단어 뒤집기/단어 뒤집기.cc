3d42f6d218c5df8d6449b81c595c00576967e828
Sang <106306044+zzzz955@users.noreply.github.com>
2024-07-22 22:36:27 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 28 ms, Memory: 2024 KB -BaekjoonHub

--- C++17/백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.cc
+++ C++17/백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.cc
@@ -1,24 +1,40 @@
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <algorithm>
 
 using namespace std;
 
 int main() {
-	int t;
-	cin >> t;
-	cin.ignore();
-	while (t--) {
-		string s;
-		getline(cin, s);
-		stringstream ss(s);
-		string ans = "";
-		string temp;
-		while (getline(ss, temp, ' ')) {
-			reverse(temp.begin(), temp.end());
-			ans += temp + " ";
-		}
-		cout << ans << "\n";
-	}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        string s;
+        getline(cin, s);
+
+        string ans;
+        string temp;
+
+        for (char c : s) {
+            if (c == ' ') {
+                reverse(temp.begin(), temp.end());
+                ans += temp + ' ';
+                temp.clear();
+            }
+            else {
+                temp += c;
+            }
+        }
+
+        reverse(temp.begin(), temp.end());
+        ans += temp;
+
+        cout << ans << "\n";
+    }
+
+    return 0;
 }
