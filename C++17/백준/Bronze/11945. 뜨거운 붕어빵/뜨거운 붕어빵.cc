e707c3299ca000de4a8e4a4021fb3de503fec1df
Sang <106306044+zzzz955@users.noreply.github.com>
2024-07-25 20:56:42 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	while (n--) {
+		string s;
+		cin >> s;
+		reverse(s.begin(), s.end());
+		cout << s << "\n";
+	}
+}
