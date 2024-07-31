8fb04566688cf9ab3b8bfe8e21ad599c521e3f70
Sang <106306044+zzzz955@users.noreply.github.com>
2024-07-31 22:20:21 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Gold/1068. 트리/트리.cc
+++ C++17/백준/Gold/1068. 트리/트리.cc
@@ -6,12 +6,10 @@ using namespace std;
 
 int n, e, rt, cnt;
 vector<vector<int>> lst;
-vector<int> v;
 
 void dfs(int index) {
 	if (lst[index].empty()) return;
 	for (int i : lst[index]) {
-		if (v[i]) continue;
 		if (i == e) {
 			if (lst[index].size() == 1) cnt++;
 			continue;
@@ -20,16 +18,13 @@ void dfs(int index) {
 			cnt++;
 			continue;
 		}
-		v[i] = 1;
 		dfs(i);
-		v[i] = 0;
 	}
 }
 
 int main() {
 	cin >> n;
 	lst.resize(n);
-	v.resize(n);
 	for (int i = 0; i < n; i++) {
 		cin >> e;
 		if (e == -1) rt = i;
@@ -37,7 +32,6 @@ int main() {
 	}
 	cin >> e;
 	lst[e].clear();
-	v[rt] = 1;
 	dfs(rt);
 	cout << cnt;
 }
