3de308fa1333e96d6da0355eee5edb7dd0f0de92
Sang <106306044+zzzz955@users.noreply.github.com>
2024-08-05 01:23:09 +0900

    [Gold I] Title: 커피숍2, Time: 104 ms, Memory: 6048 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1275. 커피숍2/커피숍2.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, q;
+vector<long long> tree;
+
+void build(vector<int>& nodes, int node, int start, int end) {
+    if (start == end) {
+        tree[node] = nodes[start];
+    }
+    else {
+        int mid = (start + end) / 2;
+        build(nodes, 2 * node, start, mid);
+        build(nodes, 2 * node + 1, mid + 1, end);
+        tree[node] = tree[2 * node] + tree[2 * node + 1];
+    }
+}
+
+void update(int node, int start, int end, int idx, int value) {
+    if (start == end) {
+        tree[node] = value;
+    }
+    else {
+        int mid = (start + end) / 2;
+        if (start <= idx && idx <= mid) {
+            update(2 * node, start, mid, idx, value);
+        }
+        else {
+            update(2 * node + 1, mid + 1, end, idx, value);
+        }
+        tree[node] = tree[2 * node] + tree[2 * node + 1];
+    }
+}
+
+long long query(int node, int start, int end, int left, int right) {
+    if (right < start || end < left) {
+        return 0;
+    }
+    if (left <= start && end <= right) {
+        return tree[node];
+    }
+    int mid = (start + end) / 2;
+    long long left_min = query(2 * node, start, mid, left, right);
+    long long right_min = query(2 * node + 1, mid + 1, end, left, right);
+    return left_min + right_min;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> q;
+    vector<int> nodes;
+    tree.resize(4 * n);
+    
+    for (int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        nodes.push_back(temp);
+    }
+    build(nodes, 1, 0, n - 1);
+
+    while (q--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        if (a > b) swap(a, b);
+        cout << query(1, 0, n - 1, a - 1, b - 1) << "\n";
+        update(1, 0, n - 1, c - 1, d);
+    }
+}
