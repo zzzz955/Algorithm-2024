ae4fef1646195f466682bf9f78d621b320159bfa
Sang <106306044+zzzz955@users.noreply.github.com>
2024-08-11 22:28:48 +0900

    [Gold III] Title: 택배, Time: 28 ms, Memory: 2288 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1719. 택배/택배.cc
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+int n, m;
+vector<vector<pair<int, int>>> lst;
+
+void init() {
+    lst.resize(n + 1);
+    while (m--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        lst[a].push_back({ c, b });
+        lst[b].push_back({ c, a });
+    }
+}
+
+void bfs(int start) {
+    vector<int> dist(n + 1, 999999999);
+    vector<vector<int>> path(n + 1);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    pq.push({ 0, start });
+    dist[start] = 0;
+
+    while (!pq.empty()) {
+        pair<int, int>  cur = pq.top(); pq.pop();
+        int cd = cur.first, cn = cur.second;
+        if (dist[cn] != cd) continue;
+        for (pair<int, int> next : lst[cn]) {
+            int nd = next.first, nn = next.second;
+            if (dist[nn] > cd + nd) {
+                dist[nn] = cd + nd;
+                if (path[cn].empty()) path[nn].push_back(nn);
+                else path[nn] = path[cn];
+                pq.push({ dist[nn], nn });
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (i == start) cout << "-" << " ";
+        else cout << path[i][0] << " ";
+    }
+    cout << "\n";
+}
+
+void solution() {
+    for (int i = 1; i <= n; i++) {
+        bfs(i);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+    init();
+    solution();
+}
