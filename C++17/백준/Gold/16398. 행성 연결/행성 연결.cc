a8e9147be0b19a88485718a02176cd4ee165704c
Sang <106306044+zzzz955@users.noreply.github.com>
2024-08-14 00:01:21 +0900

    [Gold IV] Title: 행성 연결, Time: 616 ms, Memory: 11644 KB -BaekjoonHub

---
+++ C++17/백준/Gold/16398. 행성 연결/행성 연결.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+
+int nodes[100001];
+int n, m;
+
+vector<tuple<int, int, int>> lst;
+
+int Find(int a) {
+	if (nodes[a] == a) return a;
+	return nodes[a] = Find(nodes[a]);
+}
+
+void Union(int a, int b) {
+	int rootA = Find(a);
+	int rootB = Find(b);
+	if (rootA == rootB) return;
+	nodes[rootB] = rootA;
+}
+
+int main() {
+	cin >> n;
+
+	vector<tuple<int, int, int>> edges;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			int c;
+			cin >> c;
+			if (j >= i && c) edges.push_back({ c, i, j });
+		}
+	}
+	sort(edges.begin(), edges.end());
+
+	for (int i = 1; i <= n; i++) {
+		nodes[i] = i;
+	}
+
+	long long sum = 0;
+	for (int i = 0; i < edges.size(); i++) {
+		tuple<int, int, int> cur = edges[i];
+		if (Find(get<1>(cur)) == Find(get<2>(cur))) continue;
+		Union(get<1>(cur), get<2>(cur));
+		sum += get<0>(cur);
+	}
+	cout << sum;
+}
