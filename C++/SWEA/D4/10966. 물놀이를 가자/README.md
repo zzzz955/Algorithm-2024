cb5113c812b722db25c9a6f9f3174a255241e6e8
Sang <106306044+zzzz955@users.noreply.github.com>
2024-09-02 20:26:24 +0900

    [D4] Title: 물놀이를 가자, Time: 2,382 ms, Memory: 22,324 KB -BaekjoonHub

---
+++ C++/SWEA/D4/10966. 물놀이를 가자/README.md
@@ -0,0 +1,15 @@
+# [D4] 물놀이를 가자 - 10966 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXWXMZta-PsDFAST) 
+
+### 성능 요약
+
+메모리: 22,324 KB, 시간: 2,382 ms, 코드길이: 963 Bytes
+
+### 제출 일자
+
+2024-09-02 20:26
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ C++/SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<queue>
+#include<cstring>
+
+using namespace std;
+int tc, n, m, ans;
+string lst[1000];
+int v[1000][1000];
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, 0 };
+
+struct Pos {
+	int x, y;
+};
+queue<Pos> q;
+
+void init() {
+	ans = 0;
+	memset(v, -1, sizeof(v));
+}
+
+void input() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> lst[i];
+		for (int j = 0; j < m; j++) {
+			if (lst[i][j] == 'W') {
+				q.push({ i, j });
+				v[i][j] = 0;
+			}
+		}
+	}
+}
+
+void bfs() {
+	while (!q.empty()) {
+		Pos pos = q.front(); q.pop();
+		int cx = pos.x, cy = pos.y;
+		for (int i = 0; i < 4; i++) {
+			int nx = cx + dx[i], ny = cy + dy[i];
+			if (0 <= nx && nx < n && 0 <= ny && ny < m && v[nx][ny] == -1 && lst[nx][ny] == 'L') {
+				v[nx][ny] = v[cx][cy] + 1;
+				q.push({ nx, ny });
+				ans += v[nx][ny];
+			}
+		}
+	}
+}
+
+int main() {
+	cin >> tc;
+	for (int c = 1; c <= tc; c++) {
+		init();
+		input();
+		bfs();
+		cout << "#" << c << " " << ans << "\n";
+	}	
+}
