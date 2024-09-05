01a945ab92baeb181e83c61519a8534bb50c2a52
Sang <106306044+zzzz955@users.noreply.github.com>
2024-09-05 12:59:04 +0900

    [Bronze I] Title: 성 지키기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- C++17/백준/Bronze/1236. 성 지키기/성 지키기.cc
+++ C++17/백준/Bronze/1236. 성 지키기/성 지키기.cc
@@ -1,21 +1,20 @@
 #include<iostream>
 using namespace std;
-string lst[50];
-int row[50], col[50] = {0,};
-int n, m, ans = 1e9, cnt1 = 0, cnt2 = 0;
-
+string s[50];
+int r[50],c[50];
+int n,m,i,j,a=1e9,x=0,y=0;
 int main() {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		cin >> lst[i];
-		for (int j = 0; j < m; j++) {
-			if (lst[i][j] == 'X') {
-				row[i] = 1;
-				col[j] = 1;
+	cin>>n>>m;
+	for(i=0;i<n;i++){
+		cin>>s[i];
+		for(j=0;j<m;j++){
+			if(s[i][j]=='X'){
+				r[i]=1;
+				c[j]=1;
 			}
 		}
 	}
-	for (int i = 0; i < n; i++) if (!row[i]) cnt1++;
-	for (int i = 0; i < m; i++) if (!col[i]) cnt2++;
-	cout << max(cnt1, cnt2);
+	for(i=0;i<n;i++)if(!r[i])x++;
+	for(i=0;i<m;i++)if(!c[i])y++;
+	cout<<max(x, y);
 }
