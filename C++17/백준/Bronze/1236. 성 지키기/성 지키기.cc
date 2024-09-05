c5465ea5dbefa166d2518ac8e26f7f3d56b40b26
Sang <106306044+zzzz955@users.noreply.github.com>
2024-09-05 13:07:58 +0900

    [Bronze I] Title: 성 지키기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- C++17/백준/Bronze/1236. 성 지키기/성 지키기.cc
+++ C++17/백준/Bronze/1236. 성 지키기/성 지키기.cc
@@ -1,20 +1,3 @@
 #include<iostream>
-using namespace std;
-string s[50];
-int r[50],c[50];
-int n,m,i,j,a=1e9,x=0,y=0;
-int main() {
-	cin>>n>>m;
-	for(i=0;i<n;i++){
-		cin>>s[i];
-		for(j=0;j<m;j++){
-			if(s[i][j]=='X'){
-				r[i]=1;
-				c[j]=1;
-			}
-		}
-	}
-	for(i=0;i<n;i++)if(!r[i])x++;
-	for(i=0;i<m;i++)if(!c[i])y++;
-	cout<<max(x, y);
-}
+std::string s[50];int r[50],c[50],n,m,i,j,a=1e9,x=0,y=0;
+int main() {std::cin>>n>>m;for(i=0;i<n;i++){std::cin>>s[i];for(j=0;j<m;j++){if(s[i][j]=='X'){r[i]=1;c[j]=1;}}}for(i=0;i<n;i++)if(!r[i])x++;for(i=0;i<m;i++)if(!c[i])y++;printf("%d",std::max(x,y));}
