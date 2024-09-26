12185f8ea15edd807408b2743e04977dc18d1d90
Sang <106306044+zzzz955@users.noreply.github.com>
2024-09-26 14:52:29 +0900

    [Platinum IV] Title: XOR, Time: 316 ms, Memory: 19600 KB -BaekjoonHub

---
+++ C++17/백준/Platinum/14245. XOR/XOR.cc
@@ -0,0 +1,72 @@
+#include<iostream>
+
+using namespace std;
+const int MAX_N = 500000;
+int nodes[MAX_N];
+int tree[MAX_N * 4];
+int lazy[MAX_N * 4];
+int n, m;
+
+void build(int node, int start, int end) {
+	if (start == end) tree[node] = nodes[start];
+	else {
+		int mid = (start + end) / 2;
+		build(node * 2, start, mid);
+		build(node * 2 + 1, mid + 1, end);
+		tree[node] = tree[node * 2] ^ tree[node * 2 + 1];
+	}
+}
+
+void propagate(int node, int start, int end) {
+	if (lazy[node]) {
+		if ((end - start + 1) % 2) tree[node] ^= lazy[node];
+		if (start != end) {
+			lazy[node * 2] ^= lazy[node];
+			lazy[node * 2 + 1] ^= lazy[node];
+		}
+		lazy[node] = 0;
+	}
+}
+
+void update(int node, int start, int end, int L, int R, int val) {
+	propagate(node, start, end);
+	if (R < start || end < L) return;
+	if (L <= start && end <= R) {
+		lazy[node] ^= val;
+		propagate(node, start, end);
+		return;
+	}
+	int mid = (start + end) / 2;
+	update(node * 2, start, mid, L, R, val);
+	update(node * 2 + 1, mid + 1, end, L, R, val);
+	tree[node] = tree[node * 2] ^ tree[node * 2 + 1];
+}
+
+int query(int node, int start, int end, int idx) {
+	propagate(node, start, end);
+	if (start == idx && end == idx) return tree[node];
+	int mid = (start + end) / 2;
+	if (idx <= mid) return query(node * 2, start, mid, idx);
+	return query(node * 2 + 1, mid + 1, end, idx);
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> nodes[i];
+	build(1, 0, n - 1);
+	cin >> m;
+	while (m--) {
+		int order; cin >> order;
+		if (order == 1) {
+			int L, R, v; cin >> L >> R >> v;
+			update(1, 0, n - 1, L, R, v);
+		}
+		else {
+			int index; cin >> index;
+			cout << query(1, 0, n - 1, index) << "\n";
+		}
+	}
+}
