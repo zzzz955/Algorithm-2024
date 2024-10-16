aa6a71fd5ec9d45c8afa546f4098b9535010958c
Sang <106306044+zzzz955@users.noreply.github.com>
2024-10-16 12:09:02 +0900

    [D1] Title: 스탬프 찍기, Time: 99 ms, Memory: 43,496 KB -BaekjoonHub

---
+++ Python/SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.py
@@ -0,0 +1,8 @@
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+print('#' * T)
+# for test_case in range(1, T + 1):
+#     a, b = map(int, input().split())
+#     if a > b: print(f'#{test_case} >')
+#     elif a < b: print(f'#{test_case} <')
+#     else: print(f'#{test_case} =')
