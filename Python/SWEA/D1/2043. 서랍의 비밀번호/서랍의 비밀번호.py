5d7b34ea281e7eeed92d4a2576ab39751365e447
Sang <106306044+zzzz955@users.noreply.github.com>
2024-10-16 12:10:22 +0900

    [D1] Title: 서랍의 비밀번호, Time: 102 ms, Memory: 44,796 KB -BaekjoonHub

---
+++ Python/SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.py
@@ -0,0 +1,9 @@
+# T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+a, b = map(int, input().split())
+print(abs(a - b) + 1)
+# for test_case in range(1, T + 1):
+#     a, b = map(int, input().split())
+#     if a > b: print(f'#{test_case} >')
+#     elif a < b: print(f'#{test_case} <')
+#     else: print(f'#{test_case} =')
