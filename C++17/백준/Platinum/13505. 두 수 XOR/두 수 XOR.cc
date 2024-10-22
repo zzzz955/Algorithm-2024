25d9d76beb9206ee3e7627b21212f8a9a238ebd6
Sang <106306044+zzzz955@users.noreply.github.com>
2024-10-22 22:30:14 +0900

    [Platinum III] Title: 두 수 XOR, Time: 292 ms, Memory: 47220 KB -BaekjoonHub

--- C++17/백준/Platinum/13505. 두 수 XOR/두 수 XOR.cc
+++ C++17/백준/Platinum/13505. 두 수 XOR/두 수 XOR.cc
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<cstring>
 #include<cmath>
-#include<algorithm>
 
 using namespace std;
 
@@ -9,10 +8,7 @@ const int LOG = 31;
 int n, ans;
 
 struct Trie {
-    Trie* child[2];
-    Trie() {
-        memset(child, 0, sizeof(child));
-    }
+    Trie* child[2] = { 0, };
 };
 
 void Insert(Trie* node, const string& str) {
@@ -57,6 +53,7 @@ string getBit(int n) {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    cout.tie(0);
 
     Trie* root = new Trie();
     int first;
