63d2ab0ee2404601532aeb7a45d1f9986b0ac0f0
Sang <106306044+zzzz955@users.noreply.github.com>
2024-10-23 16:51:59 +0900

    [Silver II] Title: 키트 분배하기, Time: 24 ms, Memory: 6252 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Silver/24938. 키트 분배하기/키트 분배하기.cc
+++ C++17/백준/Silver/24938. 키트 분배하기/키트 분배하기.cc
@@ -1,25 +1,29 @@
 #include<iostream>
 #include<deque>
+#define ll long long
 
 using namespace std;
 
-int n, total, ans;
-int kit[200000];
+ll n, total, ans;
+ll kit[200000];
 
 struct Node{
-	int cnt, idx;
+	ll cnt, idx;
 };
 
 deque<Node> deq;
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> kit[i];
 		total += kit[i];
 	}
 
-	int div = total / n;
+	ll div = total / n;
 	for (int i = 0; i < n; i++) {
 		if (kit[i] > div) deq.push_back({ kit[i] - div, i });
 	}
@@ -28,9 +32,10 @@ int main() {
 		if (kit[i] < div) {
 			while (!deq.empty() && kit[i] < div) {
 				Node node = deq.front(); deq.pop_front();
-				int cnt = node.cnt, idx = node.idx;
-				int need = min(div - kit[i], cnt);
+				ll cnt = node.cnt, idx = node.idx;
+				ll need = min(div - kit[i], cnt);
 				kit[i] += need;
+				kit[idx] -= need;
 				ans += need * (abs(i - idx));
 				cnt -= need;
 				if (cnt) deq.push_front({ cnt, idx });
