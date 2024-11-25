62415171473dd4a420bd049babf53f2ad44d75a7
Sang <106306044+zzzz955@users.noreply.github.com>
2024-11-25 13:52:28 +0900

    [Silver II] Title: 앵무새, Time: 8 ms, Memory: 4148 KB -BaekjoonHub

--- C++17/백준/Silver/14713. 앵무새/앵무새.cc
+++ C++17/백준/Silver/14713. 앵무새/앵무새.cc
@@ -1,51 +1,39 @@
 #include<iostream>
 #include<sstream>
 #include<unordered_map>
-#include<queue>
 #include<string>
 #define pii pair<int, int>
 using namespace std;
 int n;
 int idx[101];
+string s;
+unordered_map<string, pii> dic;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	unordered_map<string, pii> dic;
-	cin >> n;
-	cin.get();
+	cin >> n; cin.get();
 	for (int i = 1; i <= n; ++i) {
 		int index = 0;
-		string s;
 		getline(cin, s);
 		stringstream ss(s);
 		while (getline(ss, s, ' ')) dic[s] = { i, index++ };
 	}
 
-	string s;
 	getline(cin, s);
 	stringstream ss(s);
-	queue<string> q;
 	while (getline(ss, s, ' ')) {
-		if (!dic[s].first) {
+		if (!dic[s].first || dic[s].second != idx[dic[s].first]) {
 			cout << "Impossible";
 			return 0;
-		}
-
-		pii d = dic[s];
-		if (d.second == idx[d.first]) {
-			idx[d.first]++;
-			dic.erase(s);
 		}
 		else {
-			cout << "Impossible";
-			return 0;
+			idx[dic[s].first]++;
+			dic.erase(s);
 		}
 	}
 
-	int flag = 1;
-	for (const auto& d : dic) if (d.second.first) flag = 0;
-	if (flag) cout << "Possible";
+	if (!dic.size()) cout << "Possible";
 	else cout << "Impossible";
 }
