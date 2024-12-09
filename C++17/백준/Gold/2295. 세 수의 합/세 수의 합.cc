24026e44904cd13a54bbdeccd779fb8cf88a3cb0
Sang <106306044+zzzz955@users.noreply.github.com>
2024-12-09 10:21:04 +0900

    [Gold IV] Title: 세 수의 합, Time: 308 ms, Memory: 25656 KB -BaekjoonHub

---
+++ C++17/백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<map>
+using namespace std;
+
+int n;
+vector<int> U;
+map<int, int> dic;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	U.resize(n);
+	for (int i = 0; i < n; ++i) cin >> U[i];
+	sort(U.begin(), U.end());
+
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+			dic[U[i] + U[j]]++;
+
+	for (int i = n - 1; i >= 0; --i) {
+		int target = U[i];
+		for (int j = 0; j < i; ++j) {
+			if (dic[target - U[j]]) {
+				cout << target;
+				return 0;
+			}
+		}
+	}
+}
