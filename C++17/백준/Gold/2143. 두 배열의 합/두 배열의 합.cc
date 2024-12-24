f61efd37dead87ddc5f105a2c52874703f74f946
Sang <106306044+zzzz955@users.noreply.github.com>
2024-12-24 14:49:50 +0900

    [Gold III] Title: 두 배열의 합, Time: 416 ms, Memory: 59252 KB -BaekjoonHub

---
+++ C++17/백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<unordered_map>
+#define ll long long
+using namespace std;
+
+int t, n, m, A, B;
+ll sumA[1000], sumB[1000], ans;
+unordered_map<int, ll> dicA, dicB;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> t;
+
+	cin >> n;
+	ll prefix = 0;
+	for (int i = 0; i < n; ++i) {
+		cin >> A;
+		prefix += A;
+		sumA[i] = prefix;
+		dicA[prefix]++;
+		for (int j = 0; j < i; ++j) dicA[sumA[i] - sumA[j]]++;
+	}
+
+	cin >> m;
+	prefix = 0;
+	for (int i = 0; i < m; ++i) {
+		cin >> B;
+		prefix += B;
+		sumB[i] = prefix;
+		dicB[prefix]++;
+		for (int j = 0; j < i; ++j) dicB[sumB[i] - sumB[j]]++;
+	}
+
+	for (const auto& d : dicA) {
+		int key = t - d.first;
+		if (dicB[key]) ans += dicB[key] * d.second;
+	}
+	cout << ans;
+}
