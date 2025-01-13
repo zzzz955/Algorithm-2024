065d9e2adfbaa396440ec02755a2e901e161e169
Sang <106306044+zzzz955@users.noreply.github.com>
2025-01-13 17:34:01 +0900

    [Gold IV] Title: 약수의 합, Time: 72 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Gold/17425. 약수의 합/약수의 합.cc
+++ 백준/Gold/17425. 약수의 합/약수의 합.cc
@@ -5,7 +5,6 @@ using namespace std;
 
 const int N = 1000001;
 int t, n;
-bool sosu[N];
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -13,17 +12,13 @@ int main() {
 	cout.tie(0);
 
 	vector<ll> F(N, 1);
-	for (int i = 2; i < N; ++i) {
-		for (int j = i; j < N; j += i) {
+	for (int i = 2; i < N; ++i)
+		for (int j = i; j < N; j += i)
 			F[j] += i;
-		}
-	}
 
 	F[0] = 0;
 	vector<ll> G(N);
-	for (int i = 1; i < N; ++i) {
-		G[i] = G[i - 1] + F[i];
-	}
+	for (int i = 1; i < N; ++i) G[i] = G[i - 1] + F[i];
 
 	cin >> t;
 	while (t--) {
