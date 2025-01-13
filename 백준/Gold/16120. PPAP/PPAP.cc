baa4b363d0394931ba19d4f2728cf8697cf0336c
Sang <106306044+zzzz955@users.noreply.github.com>
2025-01-13 12:43:37 +0900

    [Gold IV] Title: PPAP, Time: 20 ms, Memory: 5092 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.cc
+++ 백준/Gold/16120. PPAP/PPAP.cc
@@ -1,16 +1,7 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 string s, stack;
-
-void PPAP() {
-	while (stack.size() >= 4 && string(stack.end() - 4, stack.end()) == "PPAP") {
-		int len = 4;
-		while (len--) stack.pop_back();
-		stack += 'P';
-	}
-}
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -19,9 +10,12 @@ int main() {
 	cin >> s;
 	for (int i = 0; i < s.size(); ++i) {
 		stack += s[i];
-		PPAP();
+		while (s[i] == 'P' && stack.size() >= 4 && string(stack.end() - 4, stack.end()) == "PPAP") {
+			int len = 4;
+			while (len--) stack.pop_back();
+			stack += 'P';
+		}
 	}
-	PPAP();
 	if (stack == "P") cout << "PPAP";
 	else cout << "NP";
 }
