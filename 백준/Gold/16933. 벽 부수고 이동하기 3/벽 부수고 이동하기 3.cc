f3e71baf5195314f74627b733fc09f416e3df3f4
Sang <106306044+zzzz955@users.noreply.github.com>
2025-01-17 17:06:33 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 480 ms, Memory: 17172 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.cc
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.cc
@@ -8,9 +8,6 @@ string lst[1000];
 bool v[1000][1000][11];
 struct Pos {
 	int x, y, t, k, w;
-	bool operator<(const Pos& other) const {
-		return t > other.t;
-	}
 };
 int dx[] = { 0, 1, 0, -1 };
 int dy[] = { 1, 0, -1, 0 };
@@ -29,29 +26,30 @@ void print(int cx, int cy, int ct, int ck, int cw) {
 }
 
 int bfs() {
-	priority_queue<Pos> q;
+	queue<Pos> q;
 	q.push({ 0, 0, 1, 0, 0 });
 	v[0][0][0] = 1;
 
 	while (!q.empty()) {
-		Pos pos = q.top(); q.pop();
+		Pos pos = q.front(); q.pop();
 		int cx = pos.x, cy = pos.y, ct = pos.t, ck = pos.k, cw = pos.w;
 		//print(cx, cy, ct, ck, cw);
 		if (cx == n - 1 && cy == m - 1) return ct;
 
 		for (int i = 0; i < 4; ++i) {
 			int nx = cx + dx[i], ny = cy + dy[i], nt = ct + 1, nk = ck + 1, nw = cw ^ 1;
-			if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+			if (0 <= nx && nx < n && 0 <= ny && ny < m && !v[nx][ny][ck]) {
 				if (lst[nx][ny] == '0') {
-					if (v[nx][ny][ck]) continue;
 					v[nx][ny][ck] = 1;
 					q.push({ nx, ny, nt, ck, nw });
 				}
 				else {
-					if (nk > k || v[nx][ny][nk]) continue;
-					v[nx][ny][nk] = 1;
-					if (!cw) q.push({ nx, ny, nt, nk, nw });
-					else q.push({ nx, ny, nt + 1, nk, cw });
+					if (nk > k) continue;
+					if (!cw) {
+						v[nx][ny][ck] = 1;
+						q.push({ nx, ny, nt, nk, nw });
+					}
+					else q.push({ cx, cy, nt, ck, nw });
 				}
 			}
 		}
