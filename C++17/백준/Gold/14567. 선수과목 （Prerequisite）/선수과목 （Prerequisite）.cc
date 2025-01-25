36af21a3d0a75056fdb6fc193f8c966a001e5c7d
Sang <106306044+zzzz955@users.noreply.github.com>
2025-01-25 13:42:39 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 72 ms, Memory: 4956 KB -BaekjoonHub

---
+++ C++17/백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.cc
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+const int N = 1001;
+int n, m;
+int result[N];
+vector<int> lst[N];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m;
+	while (m--) {
+		int a, b; cin >> a >> b;
+		lst[a].push_back(b);
+	}
+
+	vector<int> cnt(n + 1, 0);
+	for (int i = 1; i <= n; ++i) {
+		for (int j : lst[i]) cnt[j]++;
+	}
+
+	queue<int> q;
+	for (int i = 1; i <= n; ++i) {
+		if (!cnt[i]) q.push(i);
+	}
+
+	while (!q.empty()) {
+		int cur = q.front(); q.pop();
+		result[cur]++;
+		for (int i : lst[cur]) {
+			if (!--cnt[i]) {
+				q.push(i);
+				result[i] = max(result[i], result[cur]);
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; ++i) cout << result[i] << " ";
+}
